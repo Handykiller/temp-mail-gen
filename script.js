0a05919cd91f20a0027aabf847adb45da2160633
Handy killer <135738124+Handykiller@users.noreply.github.com>
2024-06-01 16:37:18 +0300

    Update script.js

--- script.js
+++ script.js
@@ -1,5 +1,4 @@
 function generateEmail() {
-    // Simple function to generate a random email address
     const chars = 'abcdefghijklmnopqrstuvwxyz1234567890';
     let email = '';
     for (let i = 0; i < 10; i++) {
